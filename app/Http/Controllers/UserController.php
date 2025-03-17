53ce982693ecfa61aa6edb7022e419b495713066
Mohamed Hammam <143535390+Hammam378@users.noreply.github.com>
2025-03-17 10:53:48 +0400

    Add files via upload

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return response()->json([
+            'message' => 'User created successfully',
+            'User' => $user
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8'
+        ]);
+        if (!Auth::attempt($request->only('email', 'password')))
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        $user = User::where('email', $request->email)->firstOrFail();
+        $token = $user->createToken('auth_Token')->plainTextToken;
+        return response()->json([
+            'message' => 'Login successful',
+            'User' => $user,
+            'token' => $token
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Logout successful'
+        ], 200);
+    }
+}
